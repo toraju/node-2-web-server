248361b45d222672084a0ef2ec8034e2b8ceeb1c
Raju Subbanna <toraju@ymail.com>
2017-09-13 00:16:21 -0700

    Changes needed for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app= express();
 hbs.registerPartials(__dirname+'/views/partials');
@@ -53,6 +54,6 @@ app.get('/bad',(req,res)=>{
     errorMessage:'Unable to respond with request'
   });
 });
-app.listen(3000, ()=>{
-  console.log('Started webserver on port 3000');
+app.listen(port, ()=>{
+  console.log(`Started webserver on port ${port}`);
 });
